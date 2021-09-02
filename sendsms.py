786af48ce650936aa46ce8bf9b2823c8d5dea2d1
ARITRA BELEL <82683890+belelaritra@users.noreply.github.com>
2021-09-02 20:44:10 +0530

    Update sendsms.py

--- sendsms.py
+++ sendsms.py
@@ -4,20 +4,27 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = ""
-auth_token = ""
+account_sid = "_____YOUR ACCOUNT SID_____"
+auth_token = "_____YOUR ACCOUNT AUTH TOKEN_____"
 client = Client(account_sid, auth_token)
 
 
 def sendmessage(to,msg):
     message = client.messages.create(
         from_='whatsapp:+14155238886',
-        #body='Hey, I just met you, and this is crazy...',
         body=str(msg),
         status_callback='http://postb.in/1234abcd',
-        # to='whatsapp:+918420840551'
         to='whatsapp:+91'+str(to)
     )
     print(message.Status)
 
+def sendmsglocation(to,msg,latitude,longitude):
+    message = client.messages \
+        .create(
+        from_='whatsapp:+14155238886',
+        body=str(msg),
+        persistent_action=['geo:'+str(latitude)+','+str(longitude)],
+        to='whatsapp:+91'+str(to)
+    )
+    print(message.Status)
 
